cb71ef6fa0b66eaed46eef73ebd1269870212c8c
Jordi Nakahara <jordi13@my.yorku.ca>
2023-11-29 20:16:58 -0500

    Created new main, moved old to test
    I created a new main method to act as the interface to the console as required by the assignment. I moved the old main method to a test file and will transition it to junit tests later. The new main method is still missing implementation for 4 of the required methods, will add later. It also assumes that all values are strings, because I cannot read the type from the console.

--- System 1/TCAS.java
+++ System 1/TCAS.java
@@ -1,81 +1,61 @@
+import org.w3c.dom.ls.LSOutput;
+
 import java.util.*;
+
 public class TCAS {
     public static void main(String[] args) {
-
-        //Test for basic functionality
-        TCAS_Structure test1 = new TCAS_Structure<>();
-        String str1 = "Hello there!";
-        int value1 = 2003;
-        test1.put(str1, value1);
-        String output = "Value one: " + test1.get(str1);
-        System.out.println(output);
-        System.out.println("--------------------------------------------------------------------------");
-
-        //Test for the other methods----------------------
-
-        //Initialization
-        TCAS_Structure test2 = new TCAS_Structure<>();
-        String str2 = "This is a key";
-        String str3 = "This is also a key";
-        String str4 = "Is this a key?";
-        String str5 = "Ran out of funny things";
-        int value2 = 1;
-        int value3 = 44;
-        int value4 = 23234234;
-        int value5 = -1;
-        test2.put(str2, value2);
-        test2.put(str3, value3);
-        test2.put(str4, value4);
-        test2.put(str5, value5);
-
-        //Testing the methods
-        System.out.println("2nd test initial size: " + test2.size());
-
-        System.out.println("Is the 2nd test currently empty?: " + test2.isEmpty());
-        String getTest = "This is a key";
-        System.out.println("Get result when using \"" + getTest + "\": " + test2.get(getTest));
-        getTest = "This is also a key";
-        System.out.println("Get result when using \"" + getTest + "\": " + test2.get(getTest));
-        getTest = "Is this a key?";
-        System.out.println("Get result when using \"" + getTest + "\": " + test2.get(getTest));
-        getTest = "Ran out of funny things";
-        System.out.println("Get result when using \"" + getTest + "\": " + test2.get(getTest));
-        getTest = "This should return null";
-        System.out.println("Get result when using \"" + getTest + "\": " + test2.get(getTest));
+        Scanner sc = new Scanner(System.in);
+        boolean flag = true;
+        TCAS_Structure system = new TCAS_Structure<>();
 
 
-        System.out.println("Put result when using \"Ran out of funny things\" and \"72\": "
-                + test2.put("Ran out of funny things", 72));
-        System.out.println("Put result when using \"New key\" and \"72\": "
-                + test2.put("New key", 72));
+        while (flag) {
+            try {
+                String input = sc.nextLine();
+                int index = input.indexOf('(');
+                String command = "";
+                if (index != -1) {
+                    command = input.substring(0, index);
+                } else {
+                    System.out.println("Invalid command name!");
+                    continue;
+                }
 
-        System.out.println("Remove result when using \"New key\": "
-                + test2.remove("New key"));
-        System.out.println("Put result when using \"Key new\": "
-                + test2.remove("Key new"));
+                if (command.equals("size")) {
+                    System.out.println("The size of the system is: " + system.size());
+                } else if (command.equals("isEmpty")) {
+                    if (system.isEmpty()) {
+                        System.out.printf("The system is empty");
+                    } else {
+                        System.out.println("The system is not empty");
+                    }
+                } else if (command.equals("get")) {
+                    int index2 = input.indexOf('"');
+                    int index3 = input.indexOf('"', 5);
+                    String arg = input.substring(index2 + 1, index3);
+                    String output = system.get(arg).toString();
+                    System.out.println(output);
+                } else if (command.equals("put")) {
+                    int index2 = input.indexOf('"') + 1;
+                    int index3 = input.indexOf('"', 5);
+                    String inputKey = input.substring(index2, index3);
+                    index2 = input.indexOf(',') + 1;
+                    index3 = input.indexOf(')');
+                    String inputValue = input.substring(index2, index3);
+                    String output = system.put(inputKey, inputValue).toString();
+                    System.out.println("Put value of: " + output + " to the system");
+                } else if (command.equals("remove")) {
 
-        System.out.println("Keyset result with each element of the set being printed on a new line: ");
-        Set<String> keySet1 = test2.keySet();
-        Object[] keyArray1 = keySet1.toArray();
-        for (int i = 0; i < keyArray1.length; i++) {
-            System.out.println(keyArray1[i]);
-        }
+                } else if (command.equals("keySet")) {
 
-        System.out.println("Values result with each element of the collection being printed on a new line: ");
-        Collection valueSet1 = test2.values();
-        Object[] valueArray1 = valueSet1.toArray();
-        for (int i = 0; i < keyArray1.length; i++) {
-            System.out.println(valueArray1[i]);
-        }
+                } else if (command.equals("values")) {
 
-        System.out.println("Entryset result with each pair of elements being printed on a new line: ");
-        Set<Map.Entry> entrySet1 = test2.entrySet();
-        Map.Entry[] entryArray1 = new Map.Entry[entrySet1.size()];
-        entrySet1.toArray(entryArray1);
-        for (int i = 0; i < entryArray1.length; i++) {
-            Object tempKey = entryArray1[i].getKey();
-            Object tempValue = entryArray1[i].getValue();
-            System.out.println(tempKey + " " + tempValue);
+                } else if (command.equals("entrySet")) {
+
+                }
+            } catch (Exception e) {
+                System.out.println("Invalid input!");
+            }
         }
     }
 }
---
+++ System 1/Test.java
@@ -0,0 +1,82 @@
+import java.util.*;
+
+public class Test {
+    public static void main(String[] args) {
+
+        //Test for basic functionality
+        TCAS_Structure test1 = new TCAS_Structure<>();
+        String str1 = "Hello there!";
+        int value1 = 2003;
+        test1.put(str1, value1);
+        String output = "Value one: " + test1.get(str1);
+        System.out.println(output);
+        System.out.println("--------------------------------------------------------------------------");
+
+        //Test for the other methods----------------------
+
+        //Initialization
+        TCAS_Structure test2 = new TCAS_Structure<>();
+        String str2 = "This is a key";
+        String str3 = "This is also a key";
+        String str4 = "Is this a key?";
+        String str5 = "Ran out of funny things";
+        int value2 = 1;
+        int value3 = 44;
+        int value4 = 23234234;
+        int value5 = -1;
+        test2.put(str2, value2);
+        test2.put(str3, value3);
+        test2.put(str4, value4);
+        test2.put(str5, value5);
+
+        //Testing the methods
+        System.out.println("2nd test initial size: " + test2.size());
+
+        System.out.println("Is the 2nd test currently empty?: " + test2.isEmpty());
+        String getTest = "This is a key";
+        System.out.println("Get result when using \"" + getTest + "\": " + test2.get(getTest));
+        getTest = "This is also a key";
+        System.out.println("Get result when using \"" + getTest + "\": " + test2.get(getTest));
+        getTest = "Is this a key?";
+        System.out.println("Get result when using \"" + getTest + "\": " + test2.get(getTest));
+        getTest = "Ran out of funny things";
+        System.out.println("Get result when using \"" + getTest + "\": " + test2.get(getTest));
+        getTest = "This should return null";
+        System.out.println("Get result when using \"" + getTest + "\": " + test2.get(getTest));
+
+
+        System.out.println("Put result when using \"Ran out of funny things\" and \"72\": "
+                + test2.put("Ran out of funny things", 72));
+        System.out.println("Put result when using \"New key\" and \"72\": "
+                + test2.put("New key", 72));
+
+        System.out.println("Remove result when using \"New key\": "
+                + test2.remove("New key"));
+        System.out.println("Put result when using \"Key new\": "
+                + test2.remove("Key new"));
+
+        System.out.println("Keyset result with each element of the set being printed on a new line: ");
+        Set<String> keySet1 = test2.keySet();
+        Object[] keyArray1 = keySet1.toArray();
+        for (int i = 0; i < keyArray1.length; i++) {
+            System.out.println(keyArray1[i]);
+        }
+
+        System.out.println("Values result with each element of the collection being printed on a new line: ");
+        Collection valueSet1 = test2.values();
+        Object[] valueArray1 = valueSet1.toArray();
+        for (int i = 0; i < keyArray1.length; i++) {
+            System.out.println(valueArray1[i]);
+        }
+
+        System.out.println("Entryset result with each pair of elements being printed on a new line: ");
+        Set<Map.Entry> entrySet1 = test2.entrySet();
+        Map.Entry[] entryArray1 = new Map.Entry[entrySet1.size()];
+        entrySet1.toArray(entryArray1);
+        for (int i = 0; i < entryArray1.length; i++) {
+            Object tempKey = entryArray1[i].getKey();
+            Object tempValue = entryArray1[i].getValue();
+            System.out.println(tempKey + " " + tempValue);
+        }
+    }
+}
