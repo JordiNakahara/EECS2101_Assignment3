af1748eb4e83ba608beaa2a4ae2eaea174d7499d
Landon Navarre <92194808+LandonNav@users.noreply.github.com>
2023-12-05 20:18:10 -0500

    Add files via upload
    Edited Code with Tests extended for system 2, as well as some edits (mostly in system 2) and to file names. Working as-is. Documentation could be added though.

---
+++ TCAS2.java
@@ -0,0 +1,124 @@
+package a3package;
+
+import java.util.Scanner;
+
+public class TCAS2 {
+    public static void main(String[] args){
+        //TCAS_Structure_2<Integer> tcas = new TCAS_Structure_2<>("3", 3);
+        /*
+        tcas.put("2", 2);
+        tcas.put("1", 1);
+        tcas.put("4", 4);
+        tcas.put("2", 5);
+        tcas.put("0", 0);
+
+//        TREE SHOULD LOOK LIKE SUCH:
+//                        ("3", 3)
+//                       |        |
+//                    ("2",5)  ("4", 4)
+//                   |
+//                ("1", 1)
+//               |
+//            ("0", 0)
+
+
+        System.out.println(tcas.get("2")); // Should be 5
+
+        tcas.remove("3");
+
+//        TREE SHOULD LOOK NOW LIKE SUCH:
+//                        ("2", 5)
+//                       |        |
+//                    ("1",1)  ("4", 4)
+//                   |
+//                ("0", 0)
+
+
+        System.out.println(tcas.get("0")); // Should be 0
+        System.out.println("Size: " + tcas.size()); // Should be 4
+        System.out.println(tcas.keySet()); // Should be [2, 1, 0, 4]
+        System.out.println(tcas.values()); // Should be [5, 1, 0, 4]
+        System.out.println(tcas.entrySet()); // Should be {0=0, 1=1, 2=5, 4=4}, it is in this order only because
+        //                                                                      HashMap orders it on its own
+        */
+
+        TCAS_Structure_2<Integer> tcas = new TCAS_Structure_2<>();
+        Scanner sc = new Scanner(System.in);
+        boolean flag = true;
+
+        while (flag) {
+            try {
+                String input = sc.nextLine();
+                int index = input.indexOf('(');
+                String command = "";
+                if (index != -1) {
+                    command = input.substring(0, index);
+                } else {
+                    System.out.println("Invalid command name!");
+                    continue;
+                }
+
+                if (command.equals("size")) {
+                    if (input.indexOf(')') == 5){
+                        System.out.println(tcas.size());
+                    }else {
+                        System.out.println("Invalid command name!");
+                    }
+                } else if (command.equals("isEmpty")) {
+                    if (input.indexOf(')') == 8){
+                        System.out.println(tcas.isEmpty());
+                    }else {
+                        System.out.println("Invalid command name!");
+                    }
+                } else if (command.equals("get")) {
+                    if (input.endsWith(")")){
+                        int index2 = input.indexOf('"');
+                        int index3 = input.indexOf('"', 5);
+                        String arg = input.substring(index2 + 1, index3);
+                        System.out.println(tcas.get(arg));
+                    }else {
+                        System.out.println("Invalid command name!");
+                    }
+                } else if (command.equals("put")) {
+                    int index2 = input.indexOf('"') + 1;
+                    int index3 = input.indexOf('"', 5);
+                    String inputKey = input.substring(index2, index3);
+                    index2 = input.indexOf(',') + 1;
+                    index3 = input.indexOf(')');
+                    Integer inputValue = Integer.valueOf((input.substring(index2, index3)).trim());
+                    System.out.println(tcas.put(inputKey, inputValue));
+                } else if (command.equals("remove")) {
+                    int index2 = input.indexOf('"') + 1;
+                    int index3 = input.indexOf('"', 8);
+                    String inputKey = input.substring(index2, index3);
+                    System.out.println(tcas.remove(inputKey));
+                } else if (command.equals("keySet")) {
+//                    System.out.println("Keyset result with each element of the set being printed on a new line: ");
+                    if (input.indexOf(')') == 7){
+                        System.out.println(tcas.keySet());
+                    }else {
+                        System.out.println("Invalid command name!");
+                    }
+                } else if (command.equals("values")) {
+//                    System.out.println("Values result with each element of the collection being printed on a new line: ");
+                    System.out.println(tcas.values());
+                    if (input.indexOf(')') == 7){
+                        System.out.println(tcas.values());
+                    }else {
+                        System.out.println("Invalid command name!");
+                    }
+                } else if (command.equals("entrySet")) {
+//                    System.out.println("Entryset result with each pair of elements being printed on a new line: ");
+                    if (input.indexOf(')') == 9){
+                        System.out.println(tcas.entrySet());
+                    }else {
+                        System.out.println("Invalid command name!");
+                    }
+                }
+            } catch (Exception e) {
+                System.out.println(e);
+                System.out.println("Invalid input!");
+            }
+        }
+    }
+}
---
+++ TCAS_Structure.java
@@ -0,0 +1,270 @@
+package a3package;
+
+import java.util.*;
+
+/**
+ * This class is the data structure that will be used by the TCAS system. It is based on an unsorted map that uses an array list implementation.
+ *
+ * @param <V> This is the data type of the values of the structure.
+ * @author Jordi Nakahara
+ * @version 1.0
+ */
+public class TCAS_Structure<V> implements Map<String, V> {
+    ArrayList<TCAS_Entry<V>> storageSystem;
+
+    /**
+     * This method creates an empty TCAS_Structure.
+     */
+    public TCAS_Structure() {
+        this.storageSystem = new ArrayList<>();
+    }
+
+
+    /**
+     * This method states the size of a structure by reading the size of the array list.
+     *
+     * @return The size of the structure.
+     */
+    public int size() {
+        return storageSystem.size();
+    }
+
+
+    /**
+     * This method states whether the structure is empty.
+     *
+     * @return A boolean that is true when the structure has 0 elements and false otherwise.
+     */
+    public boolean isEmpty() {
+
+        return this.storageSystem.isEmpty();
+    }
+
+
+    /**
+     * This method states whether a given key is within the structure.
+     *
+     * @param key key whose presence in this map is to be tested.
+     * @return A boolean that is true if the key exists and false otherwise.
+     */
+    public boolean containsKey(Object key) {
+
+        for (int i = 0; i < this.storageSystem.size(); i++) {
+            if (this.storageSystem.get(i).getKey().equals(key)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+
+    /**
+     * This method states whether a given value is within the structure.
+     *
+     * @param value value whose presence in this map is to be tested.
+     * @return A boolean that is true if the value exists and false otherwise.
+     */
+    public boolean containsValue(Object value) {
+        String temp = value.toString();
+        for (int i = 0; i < this.storageSystem.size(); i++) {
+            if (this.storageSystem.get(i).getValue().equals(value)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * This method returns the value of a given key.
+     *
+     * @param key the key whose associated value is to be returned.
+     * @return The value associated with the key if it exists and null otherwise.
+     */
+    public V get(Object key) {
+
+        for (int i = 0; i < this.storageSystem.size(); i++) {
+            if (this.storageSystem.get(i).getKey().equals(key)) {
+                return this.storageSystem.get(i).getValue();
+            }
+        }
+
+        return null;
+
+    }
+
+
+    /**
+     * This method adds a given key-value pair into the system.
+     *
+     * @param key   key with which the specified value is to be associated.
+     * @param value value to be associated with the specified key.
+     * @return The value that was put into the structure and null if the key already exists within the structure.
+     */
+    public V put(String key, V value) {
+
+        if (this.containsKey(key)) {
+            int index = -1;
+            for (int i = 0; i < storageSystem.size(); i++) {
+                if(this.storageSystem.get(i).getKey().equals(key)) {
+                    index = i;
+                }
+            }
+            this.storageSystem.get(index).setValue(value);
+            return value;
+        }
+
+        TCAS_Entry input = new TCAS_Entry<>(key, value);
+
+        this.storageSystem.add(input);
+        return null;
+
+    }
+
+    /**
+     * This method removes a given key and its associated value from the structure.
+     *
+     * @param key key whose mapping is to be removed from the map.
+     * @return The value associated with the structure and null if the key-value pair was not removed.
+     */
+    public V remove(Object key) {
+        for (int i = 0; i < this.storageSystem.size(); i++) {
+            if (this.storageSystem.get(i).getKey().equals(key)) {
+                V output = this.storageSystem.get(i).getValue();
+                this.storageSystem.remove(i);
+                return output;
+            }
+        }
+
+        return null;
+
+    }
+
+    /**
+     * This method puts a given map into the structure.
+     *
+     * @param m mappings to be stored in this map.
+     */
+    public void putAll(Map<? extends String, ? extends V> m) {
+        Object[] keys = m.keySet().toArray();
+        V[] values = (V[]) m.values().toArray();
+
+        for (int i = 0; i < keys.length; i++) {
+            this.put(keys[i].toString(), values[i]);
+        }
+
+    }
+
+
+    /**
+     * This method removes all key-value pairs within the system, leaving an empty system.
+     */
+    public void clear() {
+        while (!this.storageSystem.isEmpty()) {
+            this.storageSystem.remove(0);
+        }
+    }
+
+
+    /**
+     * This method gives a set containing all the keys of the system.
+     *
+     * @return A set containing all the keys in the system.
+     */
+    public Set<String> keySet() {
+        Set<String> output = new HashSet<String>();
+
+        for (int i = 0; i < storageSystem.size(); i++) {
+            output.add(this.storageSystem.get(i).getKey());
+        }
+
+        return output;
+    }
+
+
+    /**
+     * This method gives the collection of the values contained in the structure.
+     *
+     * @return A collection containing the values of the structure.
+     */
+    public Collection<V> values() {
+        Collection<V> output = new ArrayList<>();
+
+        for (int i = 0; i < storageSystem.size(); i++) {
+            output.add(this.storageSystem.get(i).getValue());
+        }
+
+        return output;
+    }
+
+    /**
+     * This method gives the set of the keys contained within the structure
+     *
+     * @return A set containing the keys of the structure.
+     */
+    public Set<Entry<String, V>> entrySet() {
+        Set<Entry<String, V>> output = new HashSet<Entry<String, V>>();
+
+        for (int i = 0; i < storageSystem.size(); i++) {
+            output.add(this.storageSystem.get(i));
+        }
+
+        return output;
+    }
+
+
+}
+
+/**
+ * This class is the data type of the entries of the TCAS_Structure.
+ * The keys are of the string data type and the data type of the values is determined by "V".
+ *
+ * @param <V> The type of the value.
+ */
+class TCAS_Entry<V> implements Map.Entry<String, V> {
+    private String key;
+    private V value;
+
+    /**
+     * This method is the constructor for new entries.
+     *
+     * @param key   The key with data type string.
+     * @param value The value with data type determined by "V".
+     */
+    public TCAS_Entry(String key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    /**
+     * This method gives the key of the entry.
+     *
+     * @return The string representing the key of the entry.
+     */
+    public String getKey() {
+        return key;
+    }
+
+    /**
+     * This method gives the value of the entry.
+     *
+     * @return The value of the entry with data type determined by "V".
+     */
+    public V getValue() {
+        return value;
+    }
+
+    /**
+     * This method changes the value of the entry.
+     *
+     * @param value new value to be stored in this entry.
+     * @return The old value of the entry.
+     */
+    public V setValue(V value) {
+        V output = this.value;
+        this.value = value;
+        return output;
+    }
+
+}
